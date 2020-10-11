3c39652053191a9bd428cd7bc994d6a9e10b9f6b
rkumar <rkredux@gmail.com>
2020-10-11 16:38:40 -0400

    add routes for games per venue

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,16 +1,15 @@
 import os
 from flask import Flask
+from flask import request
 from pydruid.client import *
 from pydruid.utils.aggregators import doublesum
 from pydruid.utils.filters import Dimension
 from markupsafe import escape
-import pandas as pd
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    #boiler plate
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
@@ -20,28 +19,43 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    #client to interact with our Apache Druid cluster running on
+    #locahost and druid's historical service listenting on port 8083
     druid_client = PyDruid('http://localhost:8083', 'druid/v2/')
 
-    #routes
+
+    ####### App routes registration starts ##############
+
     @app.route('/')
     def hello():
+        """Home page route"""
         return "Hello from MLB Analytics"
 
-    # retrieve count for each strike type for all players
-    @app.route('/player_score_count')
-    def player_score_count():
+    @app.route('/games_per_venue')
+    def games_per_venue():
+        """Route to retrieve count of games per venue."""
+
         group = druid_client.groupby(
             datasource='denormalized_strike_events',
             granularity='all',
             intervals='2015-01-01/2018-01-01',
-            dimensions=["first_name", "last_name", "strike_type"],
-            aggregations={"count": doublesum("count")}
+            dimensions=["venue_name", "game_id"]
         )
-        return group.result[10]
-    
-    #count of each strike type
+        games_per_venue = {}
+        for entry in group.result:
+            venue = entry["event"]["venue_name"]
+            if venue not in games_per_venue:
+                games_per_venue[venue] = 1
+            else: 
+                games_per_venue[venue] += 1
+
+        return games_per_venue
+
+
     @app.route('/strike_type_count')
     def strike_type_count():
+        """Route to retrieve count of each strike type."""
+
         group = druid_client.groupby(
             datasource='denormalized_strike_events',
             granularity='all',
@@ -49,11 +63,32 @@ def create_app(test_config=None):
             dimensions=["strike_type"],
             aggregations={"count": doublesum("count")}
         )
-        return group.result[1]
+        strike_type_count = {}
+        for entry in group.result:
+            key = entry["event"]["strike_type"]
+            value = int(entry["event"]["count"])
+            strike_type_count[key] = value
+        return strike_type_count
+    
 
-    #total count of a type of strike event
+    @app.route('/player_strike_count')
+    def player_score_count():
+        """Route to retrieve count of each strike type per player"""
+
+        group = druid_client.groupby(
+            datasource='denormalized_strike_events',
+            granularity='all',
+            intervals='2015-01-01/2018-01-01',
+            dimensions=["first_name", "last_name", "strike_type"],
+            aggregations={"count": doublesum("count")}
+        )
+        return group.result_json
+
+        
     @app.route('/strike_type_count/<strike_event>')
     def strikes_of_a_type(strike_event):
+        """Returns total count of a type of a selected strike event."""
+
         group = druid_client.groupby(
             datasource='denormalized_strike_events',
             granularity='all',
@@ -62,6 +97,14 @@ def create_app(test_config=None):
             filter = Dimension("strike_type") == escape(strike_event),
             aggregations={"count": doublesum("count")}
         )
-        return group.result[0]
+
+        strike_type_count = {}
+        for entry in group.result:
+            key = entry["event"]["strike_type"]
+            value = int(entry["event"]["count"])
+            strike_type_count[key] = value
+        return strike_type_count
+
+    ####### App routes registration ends ##############
 
     return app
